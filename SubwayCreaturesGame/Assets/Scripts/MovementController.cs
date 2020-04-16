2545ba8cf4134509a5efd91f3bf9c0b51605b90c
joeyikes <63592190+joeyikes@users.noreply.github.com>
2020-04-16 13:46:54 +0200

    couple of tings
    pause added
    movement tweaking
    camera following player

--- SubwayCreaturesGame/Assets/Scripts/MovementController.cs
+++ SubwayCreaturesGame/Assets/Scripts/MovementController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,23 +8,37 @@ public class MovementController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpStrength = 10f;
     public bool isGrounded = false;
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody2D _rigidbody2D;
+
+    
+    private void Awake()
     {
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
+    
     // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
     void Update()
     {
         Jump();
+       
+    }
+
+    void Move()
+    {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
         transform.position += movement * (Time.deltaTime * moveSpeed);
     }
-
     void Jump()
     {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpStrength), ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(new Vector2(0f, jumpStrength), ForceMode2D.Impulse);
         }
     }
 }
---
+++ SubwayCreaturesGame/Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ContinueGame();
+            }
+            
+        }
+    }
+    //Note to myself: if scripts still work while timescale is 0, disable and enable them here 
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    } 
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
